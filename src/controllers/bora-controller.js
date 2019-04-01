1c5c1a81a2b63d1dfff9c75d9780028ea4801ef2
Walter Vinicius <walter_vinicius@outlook.com>
2019-04-01 20:07:45 -0300

    new endpoint created

--- src/controllers/bora-controller.js
+++ src/controllers/bora-controller.js
@@ -6,10 +6,22 @@ exports.post = async(req, res, next) => {
         await repository.create(req.body);
 
         res.status(201).send({ 
-            message : 'Parabéns BORA registrado com sucesso!' 
+            message : 'BORA registrado com sucesso!' 
         });
         
     } catch (error) {
         res.status(500).send({ message: 'Falha ao processar sua requisição.' + error });
     }
 }
+
+exports.include = async(req, res, next) => {
+    try {
+        await repository.update(req.body);
+
+        res.status(200).send({
+            message: 'Participante incluido no BORA com sucesso.'
+        });
+    } catch (error) {
+        res.status(500).send({ message: 'Falha ao processar sua requisição.' + error });
+    }
+} 
